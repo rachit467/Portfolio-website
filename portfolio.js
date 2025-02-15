efc3200a4c24387d0d97cd6c0164a358bf536746
Rachit Tachamo <tachamorachit7@gmail.com>
2025-02-15 22:53:41 +0545

    modified contact in html and js

--- portfolio.js
+++ portfolio.js
@@ -46,8 +46,8 @@ contactForm.addEventListener('submit', event => {
         return;
     }
 
-    alert('Thank you for your message! I will get back to you soon.');
-    contactForm.reset();
+    // Once validation passes, submit the form
+    contactForm.submit();
 });
 
 // Email Validation Function
